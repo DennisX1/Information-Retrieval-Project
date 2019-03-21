b360717a07a8dac5c27cc15d126e7f59394b8c0e
Roman <romanscholz@Romans-MBP.fritz.box>
2019-03-21 21:10:00 +0100

    added time measuring. PageRank is surprisingly fast. Not sure if am a genius or dumb...

--- src/LinkAnalysis/PageRank.java
+++ src/LinkAnalysis/PageRank.java
@@ -13,8 +13,8 @@ public class PageRank {
 
     public static void main(String[] args) {
         try {
-            int amount = 50;
-            Review[] dataset = UtilsJson.getReviewsFromDataset(amount, 7, UtilsJson.Dataset.AMAZON_INSTANT_VIDEO);
+            int amount = 2000;
+            Review[] dataset = UtilsJson.getReviewsFromDataset(amount, 90, UtilsJson.Dataset.AMAZON_INSTANT_VIDEO);
             Random rnd = new Random(123);
             double[][] weights = new double[amount][amount];
             double[] sum = new double[weights.length];
@@ -24,6 +24,7 @@ public class PageRank {
                     sum[i] = sum[i] + weights[i][j];
                 }
             }
+
 
             for (int i = 0; i < weights.length; i++) {
                 for (int j = 0; j < weights[i].length; j++) {
@@ -40,6 +41,9 @@ public class PageRank {
     }
 
     public static double[] performCalculations(Review[] reviews, double[][] weights) {
+        long startTime = System.currentTimeMillis();
+
+
         double[] rankNew = new double[reviews.length];
         double[] rank;
         int counter = 0;
@@ -55,7 +59,18 @@ public class PageRank {
             printRank(rankNew);
 
         } while (counter < MAX_ITERATIONS && !UtilsJson.isConverged(rank, rankNew, EPSILON));
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        printTime(elapsedTime);
         return rankNew;
+    }
+
+    private static void printTime(long elapsedTime) {
+        double seconds = elapsedTime / 1000.0;
+        int s = (int)(seconds % 60);
+        int m =(int) ((seconds / 60) % 60);
+        int h =(int) ((seconds / (60 * 60)) % 24);
+        System.out.println("\nExecution time Page Rank: ");
+        System.out.println(String.format("%d:%02d:%02d", h, m, s));
     }
 
     private static void setKnownReviews(double[] rank, Review[] reviews) {
@@ -71,7 +86,7 @@ public class PageRank {
         DecimalFormat df = new DecimalFormat("#.##");
 
         for (int i = 0; i < rank.length; i++) {
-            System.out.print(df.format(rank[i])+"|");
+            System.out.print(df.format(rank[i]) + "|");
         }
         System.out.print("]");
     }
