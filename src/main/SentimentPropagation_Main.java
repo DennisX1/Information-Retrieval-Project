bea801b416ca3b107e5d64743e16dc956633885b
Dennis <DennisKn123@gmail.com>
2019-05-13 16:26:25 +0200

    BUG! in Word embeddings fixed

--- src/main/SentimentPropagation_Main.java
+++ src/main/SentimentPropagation_Main.java
@@ -14,8 +14,8 @@ import io.UtilsJson;
  * Class containing the main method for the project for Sentiment Propagation with Link Analysis.
  */
 public class SentimentPropagation_Main {
-    private static final int QUANTITY_REVIEWS= 10;
-    private static final int PERCENTAGE_KNOWN_LABELS = 50;
+    private static final int QUANTITY_REVIEWS= 1000;
+    private static final int PERCENTAGE_KNOWN_LABELS = 80;
     private static final double EPSILON = 0.000001;
     private static final int MAX_ITERATIONS = 30;
     private static final double INIT_LABEL = 0.4;
